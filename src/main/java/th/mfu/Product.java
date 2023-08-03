732abe8f547fe778678d484cad7b0515f18577b2
dagon1234 <dagontunk1234@gmail.com>
2023-08-03 14:30:23 +0700

    product

---
+++ src/main/java/th/mfu/Product.java
@@ -0,0 +1,56 @@
+package th.mfu;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Product {
+    private long id;
+    private String name;
+
+    @JsonProperty("desc")
+    private String description;
+
+    private long price;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String description, long price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+}
